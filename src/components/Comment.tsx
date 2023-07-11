dbfec92b6253048fab9ad4b08103a66310c711b9
Henrique Alexandre <Henriquealexandredec@gmail.com>
2023-07-10 21:40:36 -0300

    Commit Social in TS

---
+++ src/components/Comment.tsx
@@ -0,0 +1,48 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+  function handleLikeComment() {
+    setLikeCount(state => {
+      return state + 1;
+    });
+  }
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/Henalecam.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Henrique Alexandre</strong>
+              <time title="23 de Março às 22:58" dateTime="2022-05-11 08:13:30">
+                Cerca de 2h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp /> Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { PencilLine } from '@phosphor-icons/react';
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
+        alt="Capa do perfil"
+      />
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/Henalecam.png" />
+        <strong>Henrique Alexandre</strong>
+        <span>Full-Stack Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
